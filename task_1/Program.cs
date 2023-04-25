a57da4abafdcbe593363ecd4707132f538847d89
RFozzy688 <pokerfozzy@gmail.com>
2023-04-25 18:13:34 +0300

    create class Point3D

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace task_1
 {
+    public class Point3D : Point2D<int>
+    {
+        public int Z { get; set; }
+        public Point3D(int x, int y, int z) : base(x, y)
+        {
+            Z = z;
+        }
+        public Point3D() : base()
+        {}
+        public override string ToString()
+        {
+            return $"x = {X}\ny = {Y}\nz = {Z}";
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Point3D point3D = new Point3D(100, -10, 50);
+            Console.WriteLine(point3D);
+
+            point3D = new Point3D();
+            Console.WriteLine(point3D);
         }
     }
 }
