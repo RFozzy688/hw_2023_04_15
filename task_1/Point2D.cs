acddc6e9a922dd48954ced8276f11a8a41f9613e
RFozzy688 <pokerfozzy@gmail.com>
2023-04-25 17:46:11 +0300

    create class Point2D

---
+++ task_1/Point2D.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    public class Point2D<T>
+    {
+        // параметр типа используется для задания типа свойства 
+        public T X { get; set; }
+        public T Y { get; set; }
+        // параметр типа используется для задания типов параметров метода 
+        public Point2D(T x, T y) 
+        { 
+            X = x;
+            Y = y; 
+        }
+        public Point2D()
+        { 
+            X = default(T);
+            Y = default(T);
+        }
+    }
+
+}
