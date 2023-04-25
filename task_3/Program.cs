68012c64bc09e9c2a9d3047b37df826210651053
RFozzy688 <pokerfozzy@gmail.com>
2023-04-25 21:26:20 +0300

    description task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Подсчитать, сколько раз каждое слово встречается в заданном тексте . Результат записать в коллекцию 
+//Dictionary<TKey, TValue>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
