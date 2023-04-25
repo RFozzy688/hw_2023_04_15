ee54c546305d2f1cd6ce20c87f174534669d3cfd
RFozzy688 <pokerfozzy@gmail.com>
2023-04-25 19:05:39 +0300

    description task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Создать обобщенный класс прямой на плоскости. В классе предусмотреть 2 поля типа обобщенной
+//точки — точки, через которые проходит прямая.
+//Реализовать в классе:
+// ■ конструктор, который принимает 2 точки
+// ■ конструктор, которые принимает 4 координаты (x и у координаты для первой и второй точки)
+// ■ метод ToString()
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task_1;
+
+namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
